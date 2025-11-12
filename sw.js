b985e1052b64887cca7dbcb76e7db24908afea46
kiranraj120-png <kiranraj120@gmail.com>
2025-11-12 16:53:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-const CACHE_NAME = 'pedicalc-v1';
+const CACHE_NAME = 'pedicalc-v2'; // Changed to v2 to force update
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/calc.html',
+  '/mlad.html'
 ];
 
 // Install Service Worker
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force this new worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
@@ -25,13 +28,13 @@ self.addEventListener('activate', (event) => {
       );
     })
   );
+  self.clients.claim(); // Take control of all clients immediately
 });
 
-// Fetch Event (Offline Strategy)
+// Fetch Event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Return cached file if found, otherwise fetch from network
       return cachedResponse || fetch(event.request);
     })
   );
